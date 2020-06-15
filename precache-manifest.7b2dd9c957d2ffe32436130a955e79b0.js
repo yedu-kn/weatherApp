587278f67969b1d0c374acd51371f14e01216e54
yedu-kn <yedukn@gmail.com>
2020-06-16 00:24:33 +0530

    Updates

---
+++ precache-manifest.7b2dd9c957d2ffe32436130a955e79b0.js
@@ -0,0 +1,94 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "055508d8110dcf232f732033a29b0be8",
+    "url": "/weatherApp/index.html"
+  },
+  {
+    "revision": "9686173fbf896a04c157",
+    "url": "/weatherApp/static/css/2.367f79b7.chunk.css"
+  },
+  {
+    "revision": "8d7eff9ab3b70b5a9aab",
+    "url": "/weatherApp/static/css/main.96eb7b9e.chunk.css"
+  },
+  {
+    "revision": "9686173fbf896a04c157",
+    "url": "/weatherApp/static/js/2.95c13ccd.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/weatherApp/static/js/2.95c13ccd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8d7eff9ab3b70b5a9aab",
+    "url": "/weatherApp/static/js/main.685f0e8b.chunk.js"
+  },
+  {
+    "revision": "3af5c72f8ea47188ffa1",
+    "url": "/weatherApp/static/js/runtime-main.d9b1fb79.js"
+  },
+  {
+    "revision": "74001f5bf2d6015d66897824f0349f0f",
+    "url": "/weatherApp/static/media/bg.74001f5b.jpg"
+  },
+  {
+    "revision": "10318718a245da9a20e60441b7074b6a",
+    "url": "/weatherApp/static/media/dayClear.10318718.jpg"
+  },
+  {
+    "revision": "00edf0568c9a47d8293f1acbe61e72e0",
+    "url": "/weatherApp/static/media/dayCloud.00edf056.jpg"
+  },
+  {
+    "revision": "d45cd58ad647ac2ed68c7c3c801e9f9d",
+    "url": "/weatherApp/static/media/dayFog.d45cd58a.jpg"
+  },
+  {
+    "revision": "85baebcdd418624d05f88d1a05d2aca1",
+    "url": "/weatherApp/static/media/dayRain.85baebcd.jpg"
+  },
+  {
+    "revision": "a3843cb9e072cd787a069bc5207b626a",
+    "url": "/weatherApp/static/media/daySnow.a3843cb9.jpg"
+  },
+  {
+    "revision": "aa6fe35aa5b23fbab30ad828f7f23f18",
+    "url": "/weatherApp/static/media/nightClear.aa6fe35a.jpg"
+  },
+  {
+    "revision": "242c93bbc3b796bb458bcbf7bb0182ca",
+    "url": "/weatherApp/static/media/nightCloud.242c93bb.jpg"
+  },
+  {
+    "revision": "e2dfe0806346c2016b716928ea7b43b9",
+    "url": "/weatherApp/static/media/nightFog.e2dfe080.jpg"
+  },
+  {
+    "revision": "e080e54630f131f7c5e54bedc46bbd1c",
+    "url": "/weatherApp/static/media/nightRain.e080e546.jpg"
+  },
+  {
+    "revision": "cb591683a2b615d8026a213a206f0305",
+    "url": "/weatherApp/static/media/nightSnow.cb591683.jpg"
+  },
+  {
+    "revision": "1cd48d78f06d33973d9d761d426e69bf",
+    "url": "/weatherApp/static/media/weathericons-regular-webfont.1cd48d78.woff2"
+  },
+  {
+    "revision": "4618f0de2a818e7ad3fe880e0b74d04a",
+    "url": "/weatherApp/static/media/weathericons-regular-webfont.4618f0de.ttf"
+  },
+  {
+    "revision": "4b658767da6bd92ce2addb3ce512784d",
+    "url": "/weatherApp/static/media/weathericons-regular-webfont.4b658767.eot"
+  },
+  {
+    "revision": "8cac70ebda3f23ce472110d9f21e8593",
+    "url": "/weatherApp/static/media/weathericons-regular-webfont.8cac70eb.woff"
+  },
+  {
+    "revision": "ecaf8b481729b18f6a8494d9f691cdae",
+    "url": "/weatherApp/static/media/weathericons-regular-webfont.ecaf8b48.svg"
+  }
+]);
